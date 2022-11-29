efc5e20ba765ae2bee65e176331be6ef58f14ea8
Mikael Wiksten <mikael.wiksten@outlook.com>
2022-11-29 19:57:12 +0200

    Remove LoRa code from main.cpp

--- main.cpp
+++ main.cpp
@@ -5,28 +5,6 @@
 #include "libs/DigitalGPIO.h"
 #include <FreeRTOS.h>
 #include <task.h>
-#include "lorawan-lib/include/pico/lorawan.h"
-#include "lorawan-lib/loraconfig.h"
-
-const struct lorawan_sx1276_settings sx1276_settings = {
-    .spi = {
-        .inst = PICO_DEFAULT_SPI_INSTANCE,
-        .mosi = PICO_DEFAULT_SPI_TX_PIN,
-        .miso = PICO_DEFAULT_SPI_RX_PIN,
-        .sck  = PICO_DEFAULT_SPI_SCK_PIN,
-        .nss  = 8
-    },
-    .reset = 9,
-    .dio0  = 7,
-    .dio1  = 10
-};
-
-const struct lorawan_otaa_settings otaa_settings = {
-    .device_eui   = LORAWAN_DEVICE_EUI,
-    .app_eui      = LORAWAN_APP_EUI,
-    .app_key      = LORAWAN_APP_KEY,
-    .channel_mask = LORAWAN_CHANNEL_MASK
-};
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
